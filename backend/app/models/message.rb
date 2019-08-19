6f8b21bbb8149224cb0d12736b796f1eb560f840
Bryan Leighton <bryan.leigh77@gmail.com>
2019-08-19 11:36:11 +0300

    identifier as primary key for message

--- backend/app/models/message.rb
+++ backend/app/models/message.rb
@@ -1,8 +1,14 @@
 class Message < ApplicationRecord
   self.primary_key = :identifier
   belongs_to :session
-
+  
+  before_validation :set_identifier
+  
   validates :text, presence: true
   validates :identifier, presence: true
   validates :identifier, uniqueness: true
+  def set_identifier
+    self.identifier = "#{Time.now.to_i}#{rand(100..999)}".to_i if identifier.nil?
+  end
+  
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_102418) do
+ActiveRecord::Schema.define(version: 2019_08_18_125634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", id: false, force: :cascade do |t|
-    t.integer "identifier"
+    t.bigint "identifier"
     t.string "text"
     t.bigint "session_id"
     t.datetime "created_at", null: false
