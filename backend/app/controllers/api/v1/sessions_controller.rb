84542f2cfc4480e4fcc87bdf95598621879c6e65
Bryan Leighton <bryan.leigh77@gmail.com>
2019-08-19 15:39:01 +0300

    sessions controller replies set up.

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,62 @@
 class Api::V1::SessionsController < ApplicationController
+  before_action :set_language, only: [:replies]
   def replies
     @session = Session.find params[:id]
     @messages = @session.messages
-    bot_reply(@messages)
+    bot_reply(@messages) unless @messages.blank? 
   end
 
   private
 
-  def bot_reply(messages)
-    binding.pry
+  def set_language
+    @wl = WhatLanguage.new(:english, :german, :spanish)
   end
+  def bot_reply(messages)
+    @formatted_messages = []
+    first_message = messages.first
+    # set language from text
+    @locale = @wl.language_iso(first_message.text)
+    salutation = Reply.find_by(short_name: "#{locale}.salutation")
+    available = Reply.find_by(short_name: "#{locale}.available")
+    if salutation.nil?
+      salutation = Reply.create!(
+        message: I18n.t('salutation', locale: @locale),
+        short_name: "#{locale}.salutation",
+        reply_to: first_message.identifier,
+        session: @session
+      )
+      @formatted_messages << salutation
+      if available.nil?
+        @formatted_messages <<
+          Reply.create!(
+            message: I18n.t('available_languages', locale: @locale),
+            short_name: "#{locale}.available",
+            session: @session
+          )
+      end
+    else
+      @formatted_messages.push(salutation, available)
+    end
+    coming_soon_messages(messages[1..-1]) if messages.length > 1
+    @formatted_messages
+  end
+
+  def coming_soon_messages(messages)
+    messages.each do |mes|
+      # skip creating new message if message already has a reply
+      reply = Reply.find_by(reply_to: mes.identifier)
+      if reply.nil?
+        @formatted_messages <<
+        Reply.create!(
+            message: I18n.t('response', locale: @locale), 
+            short_name: "#{locale}.response",
+            reply_to: mes.identifier,
+            session: @session
+        )
+      else
+        @formatted_messages << reply
+      end
+    end
+  end
+  @formatted_messages
 end
--- backend/app/views/api/v1/sessions/replies.json.jbuilder
+++ backend/app/views/api/v1/sessions/replies.json.jbuilder
@@ -1,3 +1,9 @@
-json.array! @messages do |message|
-  json.extract! message, :identifier, :text, :created_at, :updated_at
+if !@formatted_messages.blank?
+  json.array! @formatted_messages do |message|
+    json.extract! message, :message, :short_name
+    json.reply_to message[:reply_to] if message[:reply_to].present?
+    json.sent_at Time.now
+  end
+else
+  json.text "Chat is Empty."
 end
