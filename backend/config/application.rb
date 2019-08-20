166158112f525179785c13e0ace978b602b4015b
Bryan Leighton <bryan.leigh77@gmail.com>
2019-08-20 22:53:44 +0300

    routes for sessions create

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,7 +29,6 @@ module Backend
       end
     end
     
-    config.middleware.use ActionDispatch::Cookies
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do 
     namespace :v1 do
-      resources :sessions, only: [] do
+      resources :sessions, only: [:show, :create] do
         member do
           get 'replies'
-        end
-        collection do 
-          get 'session'
         end
         resources :messages, param: :identifier, only: %i[create show]
       end
