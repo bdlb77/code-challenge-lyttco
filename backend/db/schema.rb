42684600b0681ab5466591369e5c96e0e8b164eb
Bryan Leighton <bryan.leigh77@gmail.com>
2019-08-20 20:22:44 +0300

    remove text from messages

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_125113) do
+ActiveRecord::Schema.define(version: 2019_08_20_172100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", id: false, force: :cascade do |t|
     t.bigint "identifier"
-    t.string "text"
     t.bigint "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
